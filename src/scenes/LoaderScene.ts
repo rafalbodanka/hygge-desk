65626b428c742db1214e013726ff0c7a726a0874
rbod <rafal.bodanka@gmail.com>
2025-02-23 20:26:33 +0100

    fix github blob font fetching

--- src/scenes/LoaderScene.ts
+++ src/scenes/LoaderScene.ts
@@ -3,7 +3,6 @@ import { manifest } from "../assets";
 
 export class LoaderScene extends Container {
 
-    // for making our loader graphics...
     private loaderBar: Container;
     private loaderBarBoder: Graphics;
     private loaderBarFill: Graphics;
@@ -16,51 +15,40 @@ export class LoaderScene extends Container {
             this.resolveCompleted = resolve;
         })
 
-        // lets make a loader graphic:
-        const loaderBarWidth = screenWidth * 0.8; // just an auxiliar variable
-        // the fill of the bar.
+        const loaderBarWidth = screenWidth * 0.8;
         this.loaderBarFill = new Graphics();
         this.loaderBarFill.beginFill(0x008800, 1)
         this.loaderBarFill.drawRect(0, 0, loaderBarWidth, 50);
         this.loaderBarFill.endFill();
-        this.loaderBarFill.scale.x = 0; // we draw the filled bar and with scale we set the %
+        this.loaderBarFill.scale.x = 0;
 
-        // The border of the bar.
         this.loaderBarBoder = new Graphics();
         this.loaderBarBoder.lineStyle(10, 0x0, 1);
         this.loaderBarBoder.drawRect(0, 0, loaderBarWidth, 50);
 
-        // Now we keep the border and the fill in a container so we can move them together.
         this.loaderBar = new Container();
         this.loaderBar.addChild(this.loaderBarFill);
         this.loaderBar.addChild(this.loaderBarBoder);
-        //Looks complex but this just centers the bar on screen.
         this.loaderBar.position.x = (screenWidth - this.loaderBar.width) / 2; 
         this.loaderBar.position.y = (screenHeight - this.loaderBar.height) / 2;
         this.addChild(this.loaderBar);
 
         // Start loading!
         this.initializeLoader().then(() => {
-            // Remember that constructors can't be async, so we are forced to use .then(...) here!
             this.gameLoaded();
         })
     }
 
     private async initializeLoader(): Promise<void>
     {
-        await Promise.all([
-            this.loadFont("VT323"),
-            Assets.init({ manifest: manifest })
-        ]);
-        
+        await Assets.init({ manifest: manifest })
+
         const bundleIds =  manifest.bundles.map(bundle => bundle.name);
 
-        // The second parameter for `loadBundle` is a function that reports the download progress!
         await Assets.loadBundle(bundleIds, this.downloadProgress.bind(this));
     }
 
     private downloadProgress(progressRatio: number): void {
-        // progressRatio goes from 0 to 1, so set it to scale
         this.loaderBarFill.scale.x = progressRatio;
     }
 
@@ -71,31 +59,5 @@ export class LoaderScene extends Container {
 
     get loadingCompleted(): Promise<void> {
         return this.completed;
-    }
-
-    private async loadFont(fontName: string): Promise<void> {
-        return new Promise<void>((resolve) => {
-            const testElement = document.createElement("span");
-            testElement.style.fontFamily = fontName;
-            testElement.style.position = "absolute";
-            testElement.style.opacity = "0";
-            testElement.innerText = "Loading...";
-            document.body.appendChild(testElement);
-    
-            if (document.fonts) {
-                document.fonts.ready.then(() => {
-                    console.log(`${fontName} loaded`);
-                    document.body.removeChild(testElement);
-                    resolve();
-                }).catch(() => {
-                    console.warn(`Failed to load ${fontName}`);
-                    document.body.removeChild(testElement);
-                    resolve();
-                });
-            } else {
-                console.warn("document.fonts API not supported");
-                resolve();
-            }
-        });
     }
 }
